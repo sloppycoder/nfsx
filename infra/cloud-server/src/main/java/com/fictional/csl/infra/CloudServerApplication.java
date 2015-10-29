c2f9fdd7826eaf4343ef55c4f4ad6cf93979b98b
Li Lin <guru.lin@gmail.com>
2015-10-29 12:12:53 +0800

    enabled Hystrix dashboard and Turbine.

--- infra/cloud-server/src/main/java/com/fictional/csl/infra/CloudServerApplication.java
+++ infra/cloud-server/src/main/java/com/fictional/csl/infra/CloudServerApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaServer
 @EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
 public class CloudServerApplication {
 
     public static void main(String[] args) {
