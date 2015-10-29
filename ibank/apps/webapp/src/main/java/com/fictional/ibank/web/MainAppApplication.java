f2d9d238b525f99d925c5ec3700aee0b4b97b703
Li Lin <guru.lin@gmail.com>
2015-10-29 18:44:54 +0800

    login/logout works without SSO.

--- ibank/apps/webapp/src/main/java/com/fictional/ibank/web/MainAppApplication.java
+++ ibank/apps/webapp/src/main/java/com/fictional/ibank/web/MainAppApplication.java
@@ -7,15 +7,22 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.security.Principal;
@@ -25,18 +32,18 @@ import java.util.Map;
 //@EnableZuulProxy
 @Controller
 @ComponentScan
-@EnableOAuth2Sso
-@EnableEurekaClient
+//@EnableOAuth2Sso
+//@EnableEurekaClient
 @EnableFeignClients
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 public class MainAppApplication extends WebMvcConfigurerAdapter {
 
     @Autowired
     CustomerServiceProxy customerServiceProxy;
 
-    @RequestMapping(value={"/dashboard"})
-    public String showIndex(Map<String,Object> model) throws Exception {
-        model.put("customer", customerServiceProxy.getCustomerFromBackend());
+    @RequestMapping(value = {"/dashboard"})
+    public String showIndex(Map<String, Object> model) throws Exception {
+        model.put("customer", customerServiceProxy.anonymousCustomer());
         return "dashboard";
     }
 
@@ -46,12 +53,55 @@ public class MainAppApplication extends WebMvcConfigurerAdapter {
         return user;
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
     @RequestMapping("/")
-    public String index() { return "redirect:dashboard"; }
+    public String index() {
+        return "redirect:dashboard";
+    }
 
     public static void main(String[] args) {
-            SpringApplication.run(MainAppApplication.class, args);
+        SpringApplication.run(MainAppApplication.class, args);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private SecurityProperties security;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().fullyAuthenticated()
+            .and()
+                .formLogin()
+                    .loginPage("/login").failureUrl("/login?error")
+                .permitAll()
+            .and()
+                .logout().permitAll();
+            // @formatter:on
         }
 
-}
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+            // @formatter:off
+            auth.inMemoryAuthentication()
+                .withUser("admin").password("admin")
+                    .roles("ADMIN", "USER")
+            .and()
+                .withUser("user").password("user")
+                    .roles("USER");
+            // @formatter:on
+        }
 
+    }
+
+}
--- ibank/apps/webapp/src/main/resources/templates/dashboard.ftl
+++ ibank/apps/webapp/src/main/resources/templates/dashboard.ftl
@@ -3,7 +3,11 @@
 <link rel="stylesheet" href="css/wro.css"/>
 </head>
 <body>
-  <div class="form-control"> hello ${customer.name} </div>
-	<script src="js/wro.js" type="text/javascript"></script>
+  <p>hello ${customer.name} </p>
+  <form role="form" action="logout" method="post">
+      <input type="hidden" id="csrf_token" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+      <button class="btn btn-primary" type="submit">Logout</button>
+  </form>
+  <script src="js/wro.js" type="text/javascript"></script>
 </body>
 </html>
