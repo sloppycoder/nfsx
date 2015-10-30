5f0c33b44a3ffac46484ad1d21f7a89aa89c2111
Li Lin <guru.lin@gmail.com>
2015-10-30 22:41:36 +0800

    properly use @EnableJpaRepositories annotation to discover spring-data-jpa repositories.

--- ibank/daos/profile-dao/src/test/java/com/fictional/ibank/profile/test/CustomerEntityRepositoryTests.java
+++ ibank/daos/profile-dao/src/test/java/com/fictional/ibank/profile/test/CustomerEntityRepositoryTests.java
@@ -1,15 +1,11 @@
 package com.fictional.ibank.profile.test;
 
-import com.fictional.ibank.profile.ProfileDaoTestApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
--- ibank/daos/profile-dao/src/test/java/com/fictional/ibank/profile/test/ProfileDaoTestApplication.java
+++ ibank/daos/profile-dao/src/test/java/com/fictional/ibank/profile/test/ProfileDaoTestApplication.java
@@ -1,10 +1,11 @@
-package com.fictional.ibank.profile;
+package com.fictional.ibank.profile.test;
 
 import com.fictional.ibank.profile.persistence.entity.CustomerEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /*
     for some unkown reason, this class must be in this package
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses=CustomerEntity.class)
-@ComponentScan
+@ComponentScan(basePackages = "com.fictional.ibank")
+@EnableJpaRepositories("com.fictional.ibank.profile.persistence.dao")
 public class ProfileDaoTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileDaoTestApplication.class, args);
