7b85c64f7d7f307c4cb3cfaa0d3d17fcb5d71b41
Li Lin <guru.lin@gmail.com>
2015-11-30 17:21:34 +0800

    add sleep between final curl command to avoid curl return error during build.

--- infra_support/compose/wait_for_app.sh
+++ infra_support/compose/wait_for_app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo Waiting for application to boot ...
-for i in {1..20}; do 
+for i in {1..20}; do
    echo "    checking..."
-   curl -sL $PUBLIC_BASE_URL/app | grep csrf_token > /dev/null && break 
-   sleep 15 
-done 
+   curl -sL $PUBLIC_BASE_URL/app | grep csrf_token > /dev/null && break
+   sleep 15
+done
 
 # the command below determines the return status of the script
+sleep 3
 curl -sL $PUBLIC_BASE_URL/app | grep csrf_token  > /dev/null
